6e334a612e1963599b6f24880136d12f4377fac6
unknown <you@example.com>
2023-09-07 20:02:01 +0800

    create table database migration and models

--- laundry_laravel/database/migrations/2023_09_07_115148_create_promos_table.php
+++ laundry_laravel/database/migrations/2023_09_07_115148_create_promos_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('promos', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('image');
+            $table->bigInteger('shop_id');
+            $table->double('old_price');
+            $table->double('new_price');
+            $table->text('description');
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('promos');
     }
 };
